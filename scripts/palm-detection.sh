9f233d694d28d22b11104fec371bb7293f1a9fe2
Krzysztof <kjendrzyca@gmail.com>
2018-07-10 20:48:39 +0200

    Update palm detection script

--- scripts/palm-detection.sh
+++ scripts/palm-detection.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-xinput set-prop "11" "Synaptics Palm Detection" 1 
-xinput set-prop "11" "Synaptics Palm Dimensions" 7, 7
-xinput set-prop "11" "Synaptics Noise Cancellation" 30, 30
+xinput set-prop "11" "Synaptics Palm Detection" 1
+xinput set-prop "11" "Synaptics Palm Dimensions" 6, 1
+xinput set-prop "11" "Synaptics Noise Cancellation" 27, 27
+xinput set-prop "11" "Synaptics Finger" 8, 16, 10
 
