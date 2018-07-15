7ed01d5709e6654624270e0d4682ac7961e8162d
kjendrzyca <kjendrzyca@gmail.com>
2018-07-15 22:31:49 +0200

    Add initial config for arch + i3

---
+++ arch/.config/i3/config.sh
@@ -0,0 +1,18 @@
+git clone https://aur.archlinux.org/package-query.git
+cd package-query
+makepkg -si
+cd ..
+git clone https://aur.archlinux.org/yaourt.git
+cd yaourt
+makepkg -si
+cd ..
+
+sudo pacman -S playerctl
+sudo pacman -S compton
+sudo pacman -S ranger w3m
+yaourt urxvt-unicode
+sudo pacman -S urxvt-perls
+yaourt light-git
+yaourt shutter
+yaourt urxvtconfig
+
---
+++ arch/.config/i3/touchpad.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env sh
+
+xinput set-prop "11" "libinput Tapping Enabled" 1
+xinput set-prop "11" "libinput Natural Scrolling Enabled" 1
