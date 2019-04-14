80d08951aa870f3204937107050a5f597a55801a
kjendrzyca <kjendrzyca@gmail.com>
2019-04-14 11:33:33 +0200

    Update i3 config after system update

--- arch/.config/i3/touchpad.sh
+++ arch/.config/i3/touchpad.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 
-xinput set-prop "10" "libinput Tapping Enabled" 1
-xinput set-prop "10" "libinput Natural Scrolling Enabled" 1
+xinput set-prop "11" "libinput Tapping Enabled" 1
+xinput set-prop "11" "libinput Natural Scrolling Enabled" 1
