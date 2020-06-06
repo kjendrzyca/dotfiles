36b9c6ea847800527ce29f7ca1ef5cae7b21022b
kjendrzyca <kjendrzyca@users.noreply.github.com>
2020-06-06 17:54:50 +0200

    Add trackpoint confg

--- arch/.config/i3/touchpad.sh
+++ arch/.config/i3/touchpad.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env sh
 
-xinput set-prop "10" "libinput Tapping Enabled" 1
-xinput set-prop "10" "libinput Natural Scrolling Enabled" 1
+xinput set-prop "11" "libinput Tapping Enabled" 1
+xinput set-prop "11" "libinput Natural Scrolling Enabled" 1
+
+xinput set-prop "10" "libinput Natural Scrolling Enabled" 0
+xinput set-prop "10" "libinput Accel Speed" 0
+xinput set-prop "10" "libinput Accel Profile Enabled" 0, 1
